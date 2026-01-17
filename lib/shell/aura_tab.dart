15ee47b685872609c305ed16fe62b1b0115eff08
Penumbra2200 <liamstewartj@gmail.com>
2026-01-17 11:43:03 -0500

    color fix

--- lib/shell/aura_tab.dart
+++ lib/shell/aura_tab.dart
@@ -10,7 +10,7 @@ class AuraTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Tab(child: ColouredTab(color: Colors.orange[200]!, text: 'Aura',),);
+    return Tab(child: ColouredTab(color: Colors.orange[800]!, text: 'Aura',),);
   }
 }
 
